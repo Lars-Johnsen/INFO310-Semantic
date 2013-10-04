45ec3d8c7fab9cec7da0f81679fdfa29de62ae56
lars.johnsen <larspettert@hotmail.com>
2013-10-04 13:44:23 +0200

    GUI

--- JsonRdf/src/gui/View.java
+++ JsonRdf/src/gui/View.java
@@ -2,9 +2,11 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -32,6 +34,39 @@ public class View extends JFrame {
 	private DefaultListModel results      = new DefaultListModel();
 	private JList            resultList   = new JList(results);
 	
+	
+	
+	/**
+	 * Creating fields for GUI elements
+	 */
+	private JLabel eventNameLabel = new JLabel("Eventname");
+	private JLabel eventNameArea = new JLabel();
+	private JPanel eventNamePanel = new JPanel();
+
+	private JLabel eventIdLabel = new JLabel("Event ID");
+	private JLabel eventIdArea = new JLabel();
+	private JPanel eventIdpanel = new JPanel();
+	
+	private JLabel headLinerLabel = new JLabel("Headliner");
+	private JLabel headlinerArea = new JLabel();
+	private JPanel headlinerpanel = new JPanel();
+	
+	private JLabel dateLabel = new JLabel("Date");
+	private JLabel DateArea = new JLabel();
+	private JPanel datePanel = new JPanel();
+	
+	private JLabel venueLabel = new JLabel("Venue");
+	private JLabel venueArea = new JLabel();
+	private JPanel venuePanel = new JPanel();
+	
+	private JLabel BandWebsiteLabel = new JLabel("Bandwebsite");
+	private JLabel BandWebsiteArea = new JLabel();
+	private JPanel BandWebsitePanel = new JPanel();
+	
+	private JLabel eventWebsiteLabel = new JLabel("Bandwebsite");
+	private JLabel eventWebsiteArea = new JLabel();
+	private JPanel eventWebsitePanel = new JPanel();
+	
 	public View(){
 		setupInterFace();
 	}
@@ -49,6 +84,7 @@ public class View extends JFrame {
 		eastPanel.removeAll();
 		eastPanel.setPreferredSize(new Dimension(512,384));
 		eastPanel.setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, Color.BLACK));
+		eastPanel.setLayout(new BoxLayout(eastPanel,BoxLayout.Y_AXIS));
 		
 		westPanel.removeAll();
 		westPanel.setPreferredSize(new Dimension(512, 384));
@@ -63,9 +99,58 @@ public class View extends JFrame {
 		}
 		JLabel imageLabel = new JLabel(new ImageIcon(ImageUtilities.resize(myPicture, 222, 120)));
 		
+		
+		
+		JLabel inputTextLabel = new JLabel("Type in your area:");
+		JPanel inputArea = new JPanel();
+		
+		
+		//Code for creating the Eventname area
+
+		eventNamePanel.add(eventNameLabel);
+		eventNamePanel.add(eventNameArea);
+		
+		//Event ID field and presentation
+
+		eventIdpanel.add(eventIdLabel);
+		eventIdpanel.add(eventIdArea);
+		
+		//headliner field and presentation
+		
+		headlinerpanel.add(headLinerLabel);
+		headlinerpanel.add(headlinerArea);
+		
+		eastPanel.add(eventNamePanel);
+		eastPanel.add(eventIdpanel);
+		
+		//date field and presentation
+		
+		datePanel.add(dateLabel);
+		datePanel.add(DateArea);
+		
+		//venue field and presentation
+		
+		venuePanel.add(venueLabel);
+		venuePanel.add(venueArea);
+		
+		//Bandwebsite field and presentation
+
+		BandWebsitePanel.add(BandWebsiteLabel);
+		BandWebsitePanel.add(BandWebsiteArea);
+		
+		
+		//Bandwebsite field and presentation
+		
+		eventWebsitePanel.add(eventWebsiteLabel);
+		eventWebsitePanel.add(eventWebsiteArea);
+		
+		inputArea.add(inputTextLabel);
+		inputArea.add(inputText);
+		inputArea.setPreferredSize(new Dimension(500, 200));
+		
 		northPanel.add(imageLabel);
 		
-		westPanel.add(inputText);
+		westPanel.add(inputArea);
 		westPanel.add(resultList);
 
 		getContentPane().setLayout(new BorderLayout());

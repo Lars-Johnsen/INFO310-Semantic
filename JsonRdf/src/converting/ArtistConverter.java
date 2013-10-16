cb666a7fa477493ce88602ad42e958ffd3f812e6
lars.johnsen <larspettert@hotmail.com>
2013-10-16 11:44:21 +0200

    Updated artistconverter to be more robust

--- JsonRdf/src/converting/ArtistConverter.java
+++ JsonRdf/src/converting/ArtistConverter.java
@@ -8,16 +8,16 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class ArtistConverter {
-	
+
 	/**
 	 * Class for converting artistInfo from JSON format to, java objects.
 	 * @author Lars Johnsen
 	 * @return 
 	 */
 
-	
+
 	public Artist convertArtist(JsonObject artistObj){
-		
+
 
 
 		JsonObject artist = artistObj.get("artist").getAsJsonObject();
@@ -31,33 +31,36 @@ public class ArtistConverter {
 		String mbID = artist.get("mbid").getAsString();
 		String artistURL = artist.get("url").getAsString();
 		//Similar comes in an array of JsonObjects as URLS to lastfm.
-		
+
 		if(artist.get("similar").isJsonObject()){
-		JsonObject similarObject = artist.get("similar").getAsJsonObject();
-		
-			JsonArray similarArtistArray = (JsonArray) similarObject.get("artist");
-			
-			for (JsonElement artistInSimilarArray : similarArtistArray){
-				JsonObject similarArtistObject = (JsonObject) artistInSimilarArray;
-				
-				similarArtistsURL.add(similarArtistObject.get("url").getAsString());
-			}	
+			JsonObject similarObject = artist.get("similar").getAsJsonObject();
+
+			if(similarObject.get("artist").isJsonArray()){
+				JsonArray similarArtistArray = (JsonArray) similarObject.get("artist");
+
+				for (JsonElement artistInSimilarArray : similarArtistArray){
+					JsonObject similarArtistObject = (JsonObject) artistInSimilarArray;
+
+					similarArtistsURL.add(similarArtistObject.get("url").getAsString());
+				}	
+			}
 		}
+		if(artist.get("tags").isJsonObject()){
 			JsonObject tagsObject = artist.get("tags").getAsJsonObject();
 			JsonArray tagsJsonArray = (JsonArray) tagsObject.get("tag");
 			for(JsonElement tagsInArtist : tagsJsonArray){
 				JsonObject tag = (JsonObject) tagsInArtist;
-				
-			
+
+
 				tagsArray.add(tag.get("url").getAsString());
-				
 			}
-			
-			Artist artistInstance = new Artist(name, mbID, similarArtistsURL, tagsArray, artistURL);
-			return artistInstance;
-		
-	}
+		}
+
+		Artist artistInstance = new Artist(name, mbID, similarArtistsURL, tagsArray, artistURL);
+		return artistInstance;
 
 	}
+
+}
 
 
